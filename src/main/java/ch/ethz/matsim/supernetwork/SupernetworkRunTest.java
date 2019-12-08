f0416b9ec096e0d8ff100eafc11c703cd1b0fc16
ubuntu18.04 <ste.penazzi1987@gmail.com>
2019-12-08 06:01:45 -0800

    siouxfalls test

--- src/main/java/ch/ethz/matsim/supernetwork/SupernetworkRunTest.java
+++ src/main/java/ch/ethz/matsim/supernetwork/SupernetworkRunTest.java
@@ -1,8 +1,30 @@
 package ch.ethz.matsim.supernetwork;
 
+import org.matsim.analysis.ScoreStatsControlerListener;
+import org.matsim.api.core.v01.Scenario;
+import org.matsim.core.config.Config;
+import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.controler.AbstractModule;
+import org.matsim.core.controler.Controler;
+import org.matsim.core.controler.TerminationCriterion;
+import org.matsim.core.scenario.ScenarioUtils;
+
+import com.google.inject.Singleton;
+
 public class SupernetworkRunTest {
 	public static void main(String[] args) {
-		System.out.println("ciao");
-	}
+		// TODO Auto-generated method stub
+				System.setProperty("matsim.preferLocalDtds", "true");
+				
+		        final Config config = ConfigUtils.loadConfig("/home/stefanopenazzi/git/supernetwork/input/siouxfalls-2014/config_default.xml");
 
+		        Scenario scenario = ScenarioUtils.loadScenario(config);
+
+		        // controler
+		        Controler controler = new Controler(scenario);
+		        
+		        System.setProperty("scenario","sbb");
+		        
+		        controler.run();
+	}
 }
