a7851edd6e922f8d0db2a8c5cc8a2a35e496a1b4
ubuntu18.04 <ste.penazzi1987@gmail.com>
2019-12-20 02:54:37 -0800

    ClusterDefaultImpl

---
+++ src/main/java/ch/ethz/matsim/supernetwork/clustering/cluster/ClusterDefaultImpl.java
@@ -0,0 +1,47 @@
+/**
+ * 
+ */
+package ch.ethz.matsim.supernetwork.clustering.cluster;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.matsim.api.core.v01.Coord;
+import org.matsim.api.core.v01.population.Activity;
+
+/**
+ * @author stefanopenazzi
+ *
+ */
+public class ClusterDefaultImpl implements Cluster {
+	
+	private final int id;
+	private List<Activity> activities;
+	private Coord centroid;
+	
+	ClusterDefaultImpl(int id,List<Activity> activities,Coord centroid){
+		this.id = id;
+		this.activities = activities;
+		this.centroid = centroid;
+	}
+	
+	public List<Activity> getActivities(){
+		return Collections.unmodifiableList(activities);
+	}
+	public int getId() {
+		return this.id;
+	}
+	public Coord getCentroid() {
+		return this.centroid;
+	}
+	public void setCentroid(Coord centroid) {
+		this.centroid = centroid;
+	}
+	public void addActivity(Activity act) {
+		activities.add(act);
+	}
+	public void removeActivity(Activity act) {
+		activities.remove(act);
+	}
+
+}
