f772144a0cfd30debe1218285e462ddf2262af0e
ubuntu18.04 <ste.penazzi1987@gmail.com>
2020-01-13 06:48:47 -0800

    SubNetwork cut by activities

--- src/main/java/ch/ethz/matsim/supernetwork/subnetwork/SubnetworkFactory.java
+++ src/main/java/ch/ethz/matsim/supernetwork/subnetwork/SubnetworkFactory.java
@@ -3,10 +3,31 @@
  */
 package ch.ethz.matsim.supernetwork.subnetwork;
 
+import java.util.List;
+
+import org.matsim.api.core.v01.network.Network;
+import org.matsim.api.core.v01.network.Node;
+import org.matsim.api.core.v01.population.Activity;
+
 /**
  * @author stefanopenazzi
  *
  */
 public class SubnetworkFactory {
+	
+	public SubnetworkFactory() {
+		
+	}
+	
+	public Network circularNetworkCutByNodes(Network father, List<Node> cutSet) {
+		Network net = null;
+		
+		return net;
+	}
 
+	public Network circularNetworkCutByActivities(Network father, List<Activity> cutSet) {
+		Network net = null;
+		
+		return net;
+	}
 }
