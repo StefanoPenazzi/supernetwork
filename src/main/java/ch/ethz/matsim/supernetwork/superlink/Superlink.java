07a1c578ffb96f960e1f0d4f1e4506bdb964e830
ubuntu18.04 <ste.penazzi1987@gmail.com>
2020-01-20 02:48:03 -0800

    Supernode interface

---
+++ src/main/java/ch/ethz/matsim/supernetwork/superlink/Superlink.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package ch.ethz.matsim.supernetwork.superlink;
+
+/**
+ * @author stefanopenazzi
+ *
+ */
+public interface Superlink {
+
+}
---
+++ src/main/java/ch/ethz/matsim/supernetwork/superlink/SuperlinkImpl.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package ch.ethz.matsim.supernetwork.superlink;
+
+/**
+ * @author stefanopenazzi
+ *
+ */
+public class SuperlinkImpl implements Superlink{
+
+}
