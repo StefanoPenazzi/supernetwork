17f314eacb8b6e167be334dd3a9acd51a90bd3c9
ubuntu18.04 <ste.penazzi1987@gmail.com>
2020-01-20 08:20:04 -0800

    ClusteringConfigGroup

---
+++ src/main/java/ch/ethz/matsim/supernetwork/modules/Config/ClusteringConfigGroup.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package ch.ethz.matsim.supernetwork.modules.Config;
+
+import org.matsim.core.config.ReflectiveConfigGroup;
+
+/**
+ * @author stefanopenazzi
+ *
+ */
+public class ClusteringConfigGroup extends ReflectiveConfigGroup {
+
+	/**
+	 * @param name
+	 */
+	public ClusteringConfigGroup(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+
+}
