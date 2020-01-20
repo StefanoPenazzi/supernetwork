1ea8b632c1494a368911c99987d20779da40ea90
ubuntu18.04 <ste.penazzi1987@gmail.com>
2020-01-20 02:54:24 -0800

    Superlink interface

--- src/main/java/ch/ethz/matsim/supernetwork/superlink/Superlink.java
+++ src/main/java/ch/ethz/matsim/supernetwork/superlink/Superlink.java
@@ -3,10 +3,16 @@
  */
 package ch.ethz.matsim.supernetwork.superlink;
 
+import ch.ethz.matsim.supernetwork.supernode.Supernode;
+
 /**
  * @author stefanopenazzi
  *
  */
 public interface Superlink {
 
+	public Supernode getFrom();
+	public Supernode getTo();
+	public double getWeight();
+	
 }
--- src/main/java/ch/ethz/matsim/supernetwork/superlink/SuperlinkImpl.java
+++ src/main/java/ch/ethz/matsim/supernetwork/superlink/SuperlinkImpl.java
@@ -3,10 +3,40 @@
  */
 package ch.ethz.matsim.supernetwork.superlink;
 
+import ch.ethz.matsim.supernetwork.supernode.Supernode;
+
 /**
  * @author stefanopenazzi
  *
  */
 public class SuperlinkImpl implements Superlink{
+
+	private Supernode from;
+	private Supernode to;
+	private double weight;
+	
+	public SuperlinkImpl(Supernode from,Supernode to,double weight) {
+		this.from = from;
+		this.to = to;
+		this.weight = weight;
+	}
+	
+	@Override
+	public Supernode getFrom() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Supernode getTo() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public double getWeight() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
