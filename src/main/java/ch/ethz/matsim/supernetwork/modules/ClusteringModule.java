bb971253441a53b8e4e560be158cfd7ca62fd264
ubuntu18.04 <ste.penazzi1987@gmail.com>
2020-01-21 02:48:22 -0800

    clustering strategies IoC

---
+++ src/main/java/ch/ethz/matsim/supernetwork/modules/ClusteringModule.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package ch.ethz.matsim.supernetwork.modules;
+
+import org.matsim.core.controler.AbstractModule;
+
+/**
+ * @author stefanopenazzi
+ *
+ */
+public class ClusteringModule extends AbstractModule {
+
+	@Override
+	public void install() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
